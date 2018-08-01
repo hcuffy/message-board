3810fdd4dddaabedb1787bb9bdc8730216a5ba25
Henry Cuffy <hcuffy_5@hotmail.com>
2018-08-01 07:39:10 +0200

    Add helmet safegaurds

--- app.js
+++ app.js
@@ -20,6 +20,8 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(helmet.dnsPrefetchControl())
+app.use(helmet.frameguard({ action: 'sameorigin' }))
 
 app.use('/', routes)
 
