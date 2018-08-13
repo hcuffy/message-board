59a37bc50cfb77190e0c99a16106587a115363e5
Henry Cuffy <hcuffy_5@hotmail.com>
2018-08-13 07:33:32 +0200

    Add thread model

---
+++ models/thread.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const ThreadSchema = new Schema(
+	{
+		thread: String,
+		password: String,
+		report: Boolean,
+		replies: [],
+		deleted:Boolean
+	},
+	{ timestamps: true }
+)
+
+const ModelClass = mongoose.model('thread', ThreadSchema)
+module.exports = ModelClass
